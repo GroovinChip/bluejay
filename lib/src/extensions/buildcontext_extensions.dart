1434dd0c43f7173c29da31e5856b795340a9bd94
Reuben Turner <groovinchip@gmail.com>
2021-02-21 17:53:52 -0500

    Some BuildContext extensions

---
+++ lib/src/extensions/buildcontext_extensions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Some shortcuts on BuildContext I use a lot
+extension BuildContextX on BuildContext {
+  /// Equivalent to `Theme.of(context)`
+  ThemeData get theme => this.theme;
+
+  /// Equivalent to `Theme.of(context).colorScheme`
+  ColorScheme get colorScheme => theme.colorScheme;
+
+  /// Equivalent to `Theme.of(context).brightness`
+  Brightness get brightness => theme.brightness;
+
+  /// True if `Theme.of(context).brightness` is `Brightness.dark`
+  bool get isDarkTheme => theme.brightness == Brightness.dark;
+}
