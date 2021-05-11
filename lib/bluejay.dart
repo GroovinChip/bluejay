1b288358c37089635007465abe3d8f0e1026a698
Reuben Turner <groovinchip@gmail.com>
2021-05-11 18:30:28 -0400

    ShakeWidget (#2)
    * Add ShakeWidget and Windows runner for example
    
    * Finish up work on ShakeWidget

--- lib/bluejay.dart
+++ lib/bluejay.dart
@@ -2,9 +2,12 @@
 
 library bluejay;
 
+export 'src/classes/sine_curve.dart';
 export 'src/extensions/extensions.dart';
 export 'src/logging/logger.dart';
 export 'src/logging/print_full_stacktraces.dart';
+export 'src/widgets/animation_controller_state.dart';
 export 'src/widgets/defer_init.dart';
+export 'src/widgets/shake_widget.dart';
 export 'src/widgets/text_editing_controller_builder.dart';
 export 'src/widgets/theme_brightness_animated_builder.dart';
---
+++ lib/src/classes/sine_curve.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+import 'package:flutter/animation.dart';
+
+/// Defines a sine curve.
+///
+/// Repeats the sine function [count] times over a 2 * pi period.
+///
+/// This class was authored by Andrea Bizzotto. Please view the original source
+/// [here](https://twitter.com/biz84/status/1392157513211121668?s=20).
+class SineCurve extends Curve {
+  SineCurve({this.count = 3});
+
+  final double count;
+
+  @override
+  double transformInternal(double t) {
+    return sin(count * 2 * pi * t);
+  }
+}
